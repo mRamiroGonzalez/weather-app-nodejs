8da1dfea1f40c36001287915c326079c26e003cd
Maxime Ramiro-Gonzalez <mramiro@pop-os.localdomain>
2020-09-11 09:42:24 +0200

    setup heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (event) => {
     errorMessage.textContent = '';
     locationMessage.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + location)
+    fetch('/weather?address=' + location)
         .then((response) => {
             response.json()
                 .then((data) => {
